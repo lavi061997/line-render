16fc818b5a637310da101fba1cfe093b868e6b98
lavi061997 <lavi061997@gmail.com>
2017-05-14 13:16:19 +0530

    Files Added

---
+++ Assets/line.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Collections.Generic;
+public class line : MonoBehaviour {
+
+	public LineRenderer linerenderer;
+	public EdgeCollider2D edgecol;
+
+	List<Vector2> points;
+
+	public void UpdateLine(Vector2 point){
+		if (points == null) {
+		
+			points = new List<Vector2> ();
+			Setpoints (point);
+			return;
+		}
+
+
+		if (Vector2.Distance(points.Last(),point) > .1f){
+		
+			Setpoints(point);
+		}
+	}
+
+		void Setpoints(Vector2 point){
+		points.Add (point);
+		linerenderer.SetPosition (points.Count - 1,point);
+
+		if (points.Count > 1) {
+			edgecol.points = points.ToArray ();
+		}
+
+		}
+}
+
---
+++ Assets/linecreator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class linecreator : MonoBehaviour {
+	public GameObject linePrefab;
+
+	line activeLine;
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetMouseButtonDown (0)) {
+			GameObject lineGO = Instantiate(linePrefab);
+			activeLine = lineGO.GetComponent<line>();
+		}
+
+		if (Input.GetMouseButtonUp (0)) {
+		
+			activeLine = null;
+		}
+		if (activeLine != null) {
+		
+			Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			activeLine.UpdateLine(mousePos);
+		}
+
+	}
+}
